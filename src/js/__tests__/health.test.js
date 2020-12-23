8d19e3a2de4d6c39c6e4485fa7608d56a3d2ea26
Elena Slyusareva <sportgirl55@bk.ru>
2020-12-23 14:19:27 +0500

    Mistakes are fixed

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,41 +1,39 @@
 import heroesSort from '../app';
 
 
-test ('greatest health toBe', () => {
+test('greatest health toBe', () => {
   const heroes = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
-  
+
   const sortedHeroes = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
 
   const result = heroesSort(heroes);
 
-  expect(result).toBe(sortedHeroes);
-})
-// toBe не работает - убедилась!!
+  expect(result).not.toBe(sortedHeroes);
+});
 
 
-test ('greatest health toEqual', () => {
+test('greatest health toEqual', () => {
   const heroes = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
-  
+
   const sortedHeroes = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
 
   const result = heroesSort(heroes);
 
   expect(result).toEqual(sortedHeroes);
-})
-
+});
